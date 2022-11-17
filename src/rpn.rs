cb5654b6bb2249153afdb18ad055c0783f3c1af9
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2022-11-17 21:41:28 +0900

    feat: rpn計算部分が完成

--- src/rpn.rs
+++ src/rpn.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 
 #[derive(Parser)]
 #[command(
@@ -16,18 +16,69 @@ struct Opts {
     formula_file: Option<String>,
 }
 
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("Invalid formula");
+                let x = stack.pop().expect("Invalid formula");
+
+                let result = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("Invalid formula"),
+                };
+                stack.push(result);
+            }
+            if self.0 {
+                println!("tokens: {:?} stack: {:?}", tokens, stack);
+            }
+        }
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("Invalid formula")
+        }
+    }
+}
+
 fn main() {
     let opts = Opts::parse();
 
-    let Some(path) = opts.formula_file else {
-        println!("No file specified");
-        return;
-    };
-    let f = File::open(path).unwrap();
-    let reader = BufReader::new(f);
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose)
+    } else {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose)
+    }
+}
 
+fn run(reader: impl BufRead, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
     for line in reader.lines() {
         let line = line.unwrap();
-        println!("{}", line);
+        let answer = calc.eval(&line);
+        println!("{}", answer);
     }
 }
